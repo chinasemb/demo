4ee34a28955769a7f4a337765400b1b7d97e2264
chinasmeb <1032944759@qq.com>
2019-06-18 20:51:24 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+<script  type="text/ecmascript-6">
+import MHeader from './components/m-header/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+@import "common/stylus/variable"
+  
+#app
+  color: $color-theme-d
+</style>
